4968f18c277a691938070fd5c599a6ee18885566
Atika Promyota <atikap37@gmail.com>
2021-04-05 16:59:36 +0700

    update

--- playground/update.php
+++ playground/update.php
@@ -1,40 +1,31 @@
 <?php
-$host = "localhost";
-$username = "root";
-$password = "";
+require_once 'connectdb.php';
 
-$database = 'codephp62';
-
-$myConn = new mysqli($host, $username, $password, $database);
-if ($myConn->connect_errno) {
-    printf("Connect failed: %s\n", $myConn->connect_error);
-    exit();
-}
- require_once 'connectdb.php';
- $id = "";
- $username = "";
- $status = "";
- if ($_SERVER["REQUEST_METHOD"] == "GET") {
+$id = "";
+$username = "";
+$status = "";
+if ($_SERVER["REQUEST_METHOD"] == "GET") {
     $id = "";
-    if(isset($_GET["id"]) && $_GET["id"] != '') {
+    if (isset($_GET["id"]) && $_GET["id"] != '') {
         $id = $_GET["id"];
         $username = $_GET["username"];
         $status = $_GET["status"];
-    }else{
-        //echo "id is null";
+    } else {
+        echo "id is null";
     }
-
-    
 }
-    
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $frmUsername = $frmPassword = "";
-    $frmUsername = $_POST["username"];
-    $frmPassword = $_POST["password"];
-    if ($frmUsername && $frmPassword) {
-        $strSQL = "UPDATE `user` SET `id`=[value-1],`username`=[value-2],`password_hash`=[value-3],`status`=[value-4] WHERE id=".$id;
-
-
+    $username = $status = "";
+    $id = $_GET["id"];
+    $username = $_POST["username"];
+    $status = $_POST["status"];
+    //echo $username . " -- " .$status;
+    $strSQL = "UPDATE user SET username='" .$username ."',status=".$status." WHERE id=".$id;
+    if (($username == "") && ($status == "")) {
+        echo "ไม่สามารถเพิ่มข้อมูลได้";
+    }else{
+        echo $strSQL;
         $result = $myConn->query($strSQL);
         if ($result) {
             echo "เพิ่มข้อมูลสำเร็จ";
@@ -53,22 +44,23 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
     <title>Document</title>
 </head>
 
 <body>
-    <form action="update.php" method="post">
+    <form action="update.php?id=<?=$id?>" method="post">
         <table border="1">
             <tr>
-                <td>username</td>
-                <td><input type="text" name="username"value="<?=$username?>"></td>
+                <td>Username</td>
+                <td><input type="text" name="username" value="<?=$username?>" ></td>
             </tr>
             <tr>
-                <td>status</td>
-                <td><input type="text" name="status"value="<?=$status?>"></td>
+                <td>Status</td>
+                <td><input type="taxt" name="status" value="<?=$status?>" ></td>
             </tr>
             <tr>
-                <td colspan="2"><input type="submit" value="save"></td>
+                <td colspan="2"><input type="submit" value="save"></td> 
             </tr>
         </table>
     </form>
