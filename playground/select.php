c5812f7edaf42fa33e5e5cf3ef7ada8eed5e3e50
Atika Promyota <atikap37@gmail.com>
2021-04-10 01:24:50 +0700

    select.php

--- playground/select.php
+++ playground/select.php
@@ -1,46 +1,57 @@
 <?php
+include 'template/header.html';
 require_once 'connectdb.php';
 
-$statement = "SELECT * FROM user";
-$mysqli_statement = $myConn->query($statement);
-// while ($row = $mysqli_statement->fetch_array()) {
-//     echo $row["id"] . ' ' . $row["username"] . ' ' . $row["status"] . '<br>';
-// }
-?>
-<!DOCTYPE html>
-<html lang="en">
+$strSQL = "SELECT `userID`, `userName`,`userStatus` FROM `user`";
+$result = $myconn->query($strSQL);
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.3/css/all.css" integrity="sha384-SZXxX4whJ79/gErwcOYf+zWLeJdY/qpuqC4cAa9rOGUstPomtqpuNWT9wdPEn2fk" crossorigin="anonymous">
-    <title>Document</title>
-</head>
+?>
+
+
+
 
 <body>
-    <table style="width: 100%;" border="1">
+   
+
+    <table class="table table-dark">
+    <thead>
         <tr>
-            <td>รหัส</td>
-            <td>ชื้อผู้ใช้</td>
+            <td>ลำดับ</td>
+            <td>ชื่อผู้ใช้</td>
             <td>สถานะ</td>
-            <td>แก้ไข</td>
+            <td>เเก้ไข</td>
             <td>ลบ</td>
         </tr>
+        </thead>
+        <tbody>
+    <?php
+    while ($row = $result->fetch_array()) {
+        //  echo $row["userName"] . "<br>";
+         ?>
+         <tr>
+        <td><?php echo $row ["userID"] ?></td>
+        <td><?php echo $row ["userName"]?> </td>
+        <td><?php echo $row ["userStatus"] ?></td>
+       
+        <td><a href="update.php?id=<?= $row["userID"]?>&userName=<?= $row["userName"]?> &userStatus=<?=$row["userStatus"]?>"><i class="fas fa-pencil-alt"></i></i></a></td>
+        <td><a href="delete.php?id=<?= $row["userID"] ?>"><i class="fas fa-trash-alt"></i></a></td>
+        
+
+       
+       
+       
+         </tr>
         <?php
-        while ($row = $mysqli_statement->fetch_array()) {
-        ?>
-        <tr>
-            <td><?php echo $row["id"]?></td>
-            <td><?php echo $row["username"]?></td>
-            <td><?php echo $row["status"]?></td>
-            <td><a href="update.php?id=<?php echo $row["id"]?>&username=<?php echo $row["username"]?>&status=<?php echo $row["status"]?>">edit</a></td>
-            <td><a href="delete.php?id=<?= $row["id"]?>"><i class="fas fa-trash-alt"></i></a></td>
-        </tr>
-        <?php
-        }
-        ?>
+    
+    }
+?>
+
+    </tbody>
     </table>
+    <a href="insert.php">add user</a>
+    <?php
+    include 'template/footer.html';
+    ?>
 </body>
 
 </html>
--- playground/update.php
+++ playground/update.php
@@ -1,69 +1,79 @@
 <?php
+include 'template/header.html';
 require_once 'connectdb.php';
 
-$id = "";
-$username = "";
-$status = "";
+$userID = "";
+$userName = "";
+$userStatus = "";
+
 if ($_SERVER["REQUEST_METHOD"] == "GET") {
     $id = "";
     if (isset($_GET["id"]) && $_GET["id"] != '') {
-        $id = $_GET["id"];
-        $username = $_GET["username"];
-        $status = $_GET["status"];
+        $userID  = $_GET["id"];
+        $userName = $_GET["userName"];
+        $userStatus = $_GET["userStatus"];
     } else {
-        echo "id is null";
+        echo "ID User is null";
     }
 }
-
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $username = $status = "";
-    $id = $_GET["id"];
-    $username = $_POST["username"];
-    $status = $_POST["status"];
-    //echo $username . " -- " .$status;
-    $strSQL = "UPDATE user SET username='" .$username ."',status=".$status." WHERE id=".$id;
-    if (($username == "") && ($status == "")) {
-        echo "ไม่สามารถเพิ่มข้อมูลได้";
-    }else{
-        echo $strSQL;
-        $result = $myConn->query($strSQL);
+    $userName = $userStatus = "";
+    $userID  = $_GET["id"];
+    $userName  = $_POST["userName"];
+    $userStatus = $_POST["userStatus"];
+
+
+    if ($userName && $userStatus) {
+        $strSQL = "UPDATE user SET userName ='" . $userName . "' , userStatus=" . $userStatus . " WHERE userID = " . $userID;
+        if (($userName == "") && ($userStatus == "")) {
+            echo "can't add information";
+        }
+
+        $result = $myconn->query($strSQL);
         if ($result) {
-            echo "เพิ่มข้อมูลสำเร็จ";
+            echo "update User Complete";
         } else {
-            echo "ไม่สามารถเพิ่มข้อมูลได้";
+            echo "update User failled";
         }
     }
 }
-
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
-    <title>Document</title>
-</head>
 
 <body>
-    <form action="update.php?id=<?=$id?>" method="post">
-        <table border="1">
-            <tr>
-                <td>Username</td>
-                <td><input type="text" name="username" value="<?=$username?>" ></td>
+
+<form action="update.php?id=<?= $userID ?>" method="post">
+    <div class="form-group">
+    <label for="exampleInputEmail1">username</label>
+    <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name=" userName" value="<?= $userName ?>">
+    </div>
+    <div class="form-group">
+    <label for="exampleInputPassword1">userStatus</label>
+    <input type="text" class="form-control" id="exampleInputPassword1" name="userStatus" value="<?= $userStatus ?>">
+  </div>
+  <button type="submit" class="btn btn-primary">save</button>
+</form>
+
+ 
+            <!-- <tr>
+                <td>userName</td>
+                <td><input type="text" name="userName" value="<?= $userName ?>"></td>
             </tr>
             <tr>
-                <td>Status</td>
-                <td><input type="taxt" name="status" value="<?=$status?>" ></td>
+                <td>userStatus</td>
+                <td><input type="text" name="userStatus" value="<?= $userStatus ?>"></td>
             </tr>
             <tr>
-                <td colspan="2"><input type="submit" value="save"></td> 
+                <td colspan="2"><input type="submit" value="save"></td>
             </tr>
         </table>
-    </form>
+    </form> -->
+
+
+
+    <?php
+    include 'template/footer.html';
+    ?>
 </body>
 
 </html>
